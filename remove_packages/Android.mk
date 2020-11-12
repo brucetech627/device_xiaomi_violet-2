e3eb4cd3b6ddce9e4466201c675f8705d49ad2c2
princejb786 <sam.jbjin@gmail.com>
2020-11-12 15:51:10 +0000

    violet: Remove more blots.
    Signed-off-by: princejb786 <sam.jbjin@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Youtube YoutubeMusicPrebuilt Drive GoogleCamera GoogleTTS Music PrebuiltGmail RecorderPrebuilt MicropaperPrebuilt NgaResources 
+LOCAL_OVERRIDES_PACKAGES := Maps Youtube YoutubeMusicPrebuilt Drive GoogleCamera GoogleTTS Music PrebuiltGmail RecorderPrebuilt MicropaperPrebuilt NgaResources arcore talkback SoundAmplifierPrebuilt DevicePolicyPrebuilt AndroidAutoStubPrebuilt GCS SafetyHubPrebuilt 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
